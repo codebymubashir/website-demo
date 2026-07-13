57705bda3682a1366b4cec6e9ff06d483381314b
codebymubashir <unimubashir2@gmail.com>
2026-07-13 01:17:37 -0700

    java updated

--- JS/first.js
+++ JS/first.js
@@ -1,1 +1,32 @@
-console.log("mubashir ")
+// let age = 22
+// console.log(age)
+
+
+
+// let age = 55;
+// if(age<=55){
+//     console.log("you are eligible for vote")
+// }else if(age>55) {
+//         console.log("you are not eligble for vote")
+//     }else{
+//         console.log("you are an idiot")
+//     }
+
+
+// let num = 7;
+// if(num%2 === 0){
+//     console.log("this is an even number")
+// }else{
+//     console.log("this is an odd number")
+// }
+
+// for(let i=1; i<=10; i++){
+//     console.log("code run")
+// }
+
+let sum = 0;
+for(let i=1; i<=50; i++){
+    sum += i;
+}
+console.log(sum);
+
